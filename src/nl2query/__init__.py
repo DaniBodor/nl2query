94363d1b805ea6cfc1d9d8511ebb6d3ca23f4a5f
DaniBodor <dani.bodor@kb.nl>
2026-03-02 13:56:31 +0100

    update settings

--- src/nl2query/__init__.py
+++ src/nl2query/__init__.py
@@ -1,9 +1,8 @@
 """Documentation about nl2query."""
 
 import logging
+from importlib.metadata import version
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__author__ = "Dani Bodor"
-__email__ = "dani.bodor@kb.nl"
-__version__ = "0.1.0"
+__version__ = version("nl2query")
